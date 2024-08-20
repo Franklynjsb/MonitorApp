508c16d424645e29603d1abd0b6cdd779c2cb92d
Frank♥ <98553892+Franklynjsb@users.noreply.github.com>
2024-08-20 19:18:24 -0300

    Add: Componentes necesarios para la creacion de la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiderbarComponent } from './components/siderbar/siderbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TitleComponent } from './components/title/title.component';
+import { HeaderIconsComponent } from './components/header-icons/header-icons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiderbarComponent,
+    HeaderComponent,
+    TitleComponent,
+    HeaderIconsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header-icons/header-icons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-icons',
+  templateUrl: './header-icons.component.html',
+  styleUrl: './header-icons.component.css'
+})
+export class HeaderIconsComponent {
+
+}
