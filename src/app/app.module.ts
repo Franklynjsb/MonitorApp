269f15ac5281162721758b01370c0261cbae4204
Frank♥ <98553892+Franklynjsb@users.noreply.github.com>
2024-08-21 18:08:18 -0300

    Add: Archivos de estructura FrontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SiderbarComponent } from './components/siderbar/siderbar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { TitleComponent } from './components/title/title.component';
-import { HeaderIconsComponent } from './components/header-icons/header-icons.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TopComponent } from './components/top/top.component';
+import { ItemsComponent } from './components/items/items.component';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { IndicadoresComponent } from './components/indicadores/indicadores.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SiderbarComponent,
-    HeaderComponent,
-    TitleComponent,
-    HeaderIconsComponent
+    DashboardComponent,
+    TopComponent,
+    ItemsComponent,
+    TablaComponent,
+    IndicadoresComponent
   ],
   imports: [
     BrowserModule,
